e4868bc2fea663b7693eb5ebea76bcb042642077
Alan Vieceli <alansvieceli@gmail.com>
2019-06-06 22:56:55 -0300

    Configurando as Rotas (React-Router)

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,14 +4,12 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 //moduloes foi declarado no webpack.config.js
 
 import React from 'react'
-import Tarefa from '../tarefas/tarefas'
-import Sobre from '../sobre/sobre'
 import Menu from '../template/menu';
+import Routes from './routes'
 
 export default props => (
     <div className='container'>
         <Menu />
-        <Tarefa />
-        <Sobre />
+        <Routes />
     </div>
 );
