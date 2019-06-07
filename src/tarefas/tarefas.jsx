5667b563e613e827fdf07892c484b1e813115059
Alan Vieceli <alansvieceli@gmail.com>
2019-06-06 23:15:09 -0300

    Componentes TodoForm e TodoList

--- src/tarefas/tarefas.jsx
+++ src/tarefas/tarefas.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TarefaForm from './tarefaForm'
+import TarefasLista from './tarefasLista'
 
 export default class Tarefa extends Component {
     render(){
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"/>
+                <TarefaForm />
+                <TarefasLista />
             </div>
         );
     }
