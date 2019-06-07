78fad6c1368903ce52e3dd599d8d79d22d535a95
Alan Vieceli <alansvieceli@gmail.com>
2019-06-07 20:27:10 -0300

    integração com o backend, inclusão de uma tarefa

--- src/tarefas/tarefas.jsx
+++ src/tarefas/tarefas.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader'
 import TarefasForm from './tarefasForm'
 import TarefasLista from './tarefasLista'
+
+const URL = "http://localhost:3003/api/tarefas";
 
 export default class Tarefa extends Component {
 
     constructor(props){
         super(props);
         this.state = {descricao: '', lista: []};
-        this.OnClick = this.OnClick.bind(this);
+        this.addTarefa = this.addTarefa.bind(this);
         this.onChange = this.onChange.bind(this);
     }
 
-    OnClick(){
-        console.log(this.state.descricao);
+    addTarefa(){
+        const descricao = this.state.descricao;
+        axios.post(URL, { descricao })
+            .then(resp => console.log("Ass"))
     }
 
     onChange(e){
@@ -27,7 +33,7 @@ export default class Tarefa extends Component {
                 <TarefasForm 
                     descricao={this.state.descricao}
                     onChange={this.onChange}
-                    OnClick={this.OnClick} />
+                    OnClick={this.addTarefa} />
                 <TarefasLista />
             </div>
         );
