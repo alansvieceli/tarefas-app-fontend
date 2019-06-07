7e76db76bcc890e7f2eb3e538770c6c4babc8d0e
Alan Vieceli <alansvieceli@gmail.com>
2019-06-07 19:54:22 -0300

    Adicionando componentes grig.jsx, iconButton.jsx e if.jsx

--- src/tarefas/tarefas.jsx
+++ src/tarefas/tarefas.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
-import TarefaForm from './tarefaForm'
+import TarefasForm from './tarefasForm'
 import TarefasLista from './tarefasLista'
 
 export default class Tarefa extends Component {
+
+    constructor(props){
+        super(props);
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        console.log(this);
+    }
+
     render(){
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"/>
-                <TarefaForm />
+                <TarefasForm handleAdd={this.handleAdd} />
                 <TarefasLista />
             </div>
         );
